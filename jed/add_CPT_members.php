1cf1a27f3ef7ba9e325e1118b78efe55c4b2d45b
jed-design <55106578+jed-design@users.noreply.github.com>
2020-04-29 23:21:14 +0200

    Add files via upload

---
+++ jed/add_CPT_members.php
@@ -0,0 +1,34 @@
+﻿?<?php
+// Our custom post type function
+// This script is copied from: https://www.wpbeginner.com/wp-tutorials/how-to-create-custom-post-types-in-wordpress/
+// WARNING! The modification may be completely wrong! 
+// *****************************************************************************************************************
+function create_posttype() {
+ 
+    register_post_type( 'members',
+    // Create Post Type Options
+        array(
+            'labels' => array(
+				'firstname' =>__( 'Fornavn' ),
+				'lastname' =>__( 'Etternavn' ),
+		    		'fullname' =>__( 'For- og Etternavn'),
+				'adress1' =>__( 'Adresse' ),
+		    		'status' =>__( 'Active-Inactive' ),
+				'email' =>__( 'E-post' ),
+				'phonemobile' =>__( 'Tlf.mobil' ),
+				'postaladdress' =>__( 'Postadresse' ),
+				'postalcode' =>__( 'Postnr' ),	
+				'userid' =>__( 'Tlf.Mobil' ),
+				'id' =>__( 'PersonId' ),
+			),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'm'),
+            'show_in_rest' => true,
+ 
+        )
+    );
+}
+// Hooking up our function to theme setup
+add_action( 'init', 'create_posttype' );
+?>
---
+++ jed/add_Custom_Post_Type_ to_wordpress.php
@@ -0,0 +1,36 @@
+ï»¿<?php
+// Our custom post type function
+// This script is copied from: https://www.wpbeginner.com/wp-tutorials/how-to-create-custom-post-types-in-wordpress/
+// this adds Custom Post Types for Booking.
+// *****************************************************************************************************************
+function create_posttype() {
+ 
+    register_post_type( 'bookings',
+    // Create Post Type Options
+	// bookings/id's: Name of the actual room. Bak kjøkkent f.eks = bk
+	// bookings/children/id: the bed in the room, example bk.1 for bed 1 in room bk.
+		
+        array(
+            'labels' => array(
+				'start' =>__( 'Start time of booking' ),
+				'end' =>__( 'End time of booking' ),
+				'resourceId' =>__( ' Booking OPK-member' ), 
+				'id' =>__( 'Room Id' ),
+				'title' =>__( 'Room Name' ),
+				'children' =>_array(
+					'id' =>__( 'bed id' ),
+					'title' =>__( 'Sengeplass' ),
+					)
+			
+			),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'm'),
+            'show_in_rest' => true,
+ 
+        )
+    );
+}
+// Hooking up our function to theme setup
+add_action( 'init', 'create_posttype' );
+?>
